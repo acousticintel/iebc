794375c78548b3a43282cfd2479c7abcd7389b47
acousticintel <acousticintelligenceafrica@gmail.com>
2021-12-21 14:41:20 +0300

    flags update

--- components/choose.js
+++ components/choose.js
@@ -1,14 +1,23 @@
-import React from 'react';
+import { useRecoilState } from "recoil";
 import Image from 'next/image';
 import { useRouter } from 'next/router';
+import { formData } from '../atoms/formAtom';
+import { useEffect, useState } from "react";
 
 export default function Choose({ servicesRef }) {
   const router = useRouter();
+  const [formdata, setFormData] = useState('');
 
   const handleClick = e => {
     e.preventDefault();
     router.push('/infoform');
   }
+
+  const handleCntryClick = cntry => {
+    setFormData(cntry);
+  }
+
+  useEffect(()=>{console.log(formData)},[formData])
 
   return (
     <>
@@ -23,32 +32,42 @@ export default function Choose({ servicesRef }) {
             rounded-t"/>
           </div>
           <div className='w-full px-20 flex flex-wrap justify-around'>
-            <div className='flex flex-col text-gray-800 text-center'>
-              <div className=' flag'>
+            <div className='flex flex-col text-gray-800 text-center'
+              onClick={()=>handleCntryClick('uae')}
+            >
+              <div className={`flag ${formdata === 'uae' && 'selected'}`}>
                 <Image src={'/assets/uae.png'} layout='fill' />
               </div>
               <p className='text-lg mt-2 font-medium'>United Arab Emirates</p>
             </div>
-            <div className='flex flex-col text-gray-800 text-center'>
-              <div className=' flag'>
-              <Image src={'/assets/uk.png'} layout='fill' />
+            <div className='flex flex-col text-gray-800 text-center'
+            onClick={()=>handleCntryClick('uk')}
+            >
+              <div className={`flag ${formdata === 'uk' && 'selected'}`}>
+                <Image src={'/assets/uk.png'} layout='fill' />
               </div>
               <p className='text-lg mt-2 font-medium'>United Kingdom</p>
             </div>
-            <div className='flex flex-col text-gray-800 text-center'>
-              <div className=' flag'>
+            <div className='flex flex-col text-gray-800 text-center'
+            onClick={()=>handleCntryClick('us')}
+            >
+              <div className={`flag ${formdata === 'us' && 'selected'}`}>
                 <Image src={'/assets/us.png'} layout='fill' />
               </div>
               <p className='text-lg mt-2 font-medium'>United States of America</p>
             </div>
-            <div className='flex flex-col text-gray-800 text-center'>
-              <div className=' flag'>
+            <div className='flex flex-col text-gray-800 text-center'
+            onClick={()=>handleCntryClick('can')}
+            >
+              <div className={`flag ${formdata === 'can' && 'selected'}`}>
                 <Image src={'/assets/canada.png'} layout='fill' />
               </div>
               <p className='text-lg mt-2 font-medium'>Canada</p>
             </div>
-            <div className='flex flex-col text-gray-800 text-center'>
-              <div className=' flag'>
+            <div className='flex flex-col text-gray-800 text-center'
+            onClick={()=>handleCntryClick('qat')}
+            >
+              <div className={`flag ${formdata === 'qat' && 'selected'}`}>
                 <Image src={'/assets/qatar.png'} layout='fill' />
               </div>
               <p className='text-lg mt-2 font-medium'>Qatar</p>
@@ -56,11 +75,11 @@ export default function Choose({ servicesRef }) {
           </div>
         </div>
       </section>
-      <section ref={servicesRef} className="bg-white py-8">
+      <section className="bg-white py-8">
         <div className="container mx-auto flex flex-wrap pt-4 pb-12">
           <h1 className="w-full my-2 text-5xl font-bold leading-tight 
         text-center text-gray-800">
-            Choose a service
+            Choose a Service
           </h1>
           <div className="w-full mb-4">
             <div className="h-1 mx-auto gradient w-64 opacity-25 my-0 py-0 
