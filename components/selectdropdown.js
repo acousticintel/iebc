1e13581d24c1bde173130cb48229f03494e0ea8b
acousticintel <acousticintelligenceafrica@gmail.com>
2021-12-22 20:42:39 +0300

    build fix

--- components/selectdropdown.js
+++ components/selectdropdown.js
@@ -8,7 +8,7 @@ export default function SelectDropdown({ value, list, change, setFunc }) {
     <>
       <div className="relative">
         <div>
-          <button type="button" class="inline-flex 
+          <button type="button" className="inline-flex 
         justify-center pt-3 w-full h-11 rounded-md border 
         border-gray-300 shadow-sm px-4 py-2 bg-white 
         text-sm font-medium text-gray-700 
@@ -21,8 +21,8 @@ export default function SelectDropdown({ value, list, change, setFunc }) {
             onBlur={() => setDropOpen(false)}
           >
             {value}
-            <svg class="-mr-1 ml-2 h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-              <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+            <svg className="-mr-1 ml-2 h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+              <path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd" />
             </svg>
           </button>
         </div>
